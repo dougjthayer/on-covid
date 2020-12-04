99492109c5e26aeb1c2f933ec063d70ebb636197
Cool Ranch <cranchy@Cools-MacBook-Pro.local>
2020-12-04 09:36:33 -0500

    slide structure

--- src/components/SimpleSlider/SimpleSlider.js
+++ src/components/SimpleSlider/SimpleSlider.js
@@ -8,6 +8,7 @@ import "slick-carousel/slick/slick-theme.css";
 class simpleSlider extends React.Component {
   render() {
     var settings = {
+        arrows: false,
         dots: true,
         infinite: true,
         speed: 500,
@@ -18,41 +19,38 @@ class simpleSlider extends React.Component {
     };
     return (
     <div>
+      <span class="report-date">{this.props.generalData.date}</span>
         <Slider {...settings}>
             <div classname="slider-slide">
-                {this.props.generalData.date} <br />
-                {this.props.generalData.newInfectionsPercentChange} <br />
-                Increase today <br />
-                {this.props.generalData.newInfectionsToday} <br />
-                New Infections <br />
+                <span class="small-stat">{this.props.generalData.newInfectionsPercentChange}</span>
+                <span class="small-title">Increase today</span>
+                <span class="big-stat">{this.props.generalData.newInfectionsToday}</span>
+                <h2>New Infections</h2>
             </div>
             <div classname="slider-slide">
-                {this.props.generalData.date} <br />
-                {this.props.generalData.testsPositivity} <br />
-                Positive Today <br />
-                {this.props.generalData.testsCompleted} <br />
-                Tests Completed <br />
+                <span class="small-stat">{this.props.generalData.testsPositivity}</span>
+                <span class="small-title">Positive Today</span>
+                <span class="big-stat">{this.props.generalData.testsCompleted}</span>
+                <h2>Tests Completed</h2>
             </div>
             <div classname="slider-slide">
-                {this.props.generalData.date} <br />
-                {this.props.generalData.deathsToday} <br />
-                Deaths Today <br />
-                {this.props.generalData.deathsTotal} <br />
-                Total Deaths <br />
+                <span class="small-stat">{this.props.generalData.deathsToday}</span>
+                <span class="small-title">Deaths Today</span>
+                <span class="big-stat">{this.props.generalData.deathsTotal}</span>
+                <h2>Total Deaths</h2>
             </div>
             <div className="slider-slide">
-                {this.props.generalData.date} <br />
-                {this.props.generalData.icu}   {this.props.generalData.ventilator} <br />
-                ICU / Ventilator <br />
-                {this.props.generalData.hospitalized} <br />
-                Hospitalized <br />
+                <span class="small-stat"><span>{this.props.generalData.icu}</span><span>{this.props.generalData.ventilator}</span></span>
+                <span class="small-title">ICU / Ventilator</span>
+                <span class="big-stat">{this.props.generalData.hospitalized}</span>
+                <h2>Hospitalized</h2>
             </div>
             <div className="slider-slide">
-                Put county data here
+                <h2>Cases by County</h2>
             </div>
         </Slider>
     </div>
-      
+
     );
   }
 }
